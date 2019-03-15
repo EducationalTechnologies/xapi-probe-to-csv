fb546dfe88ec0cac91e6364f5a2ad38549d72871
Noah Hummel <n.p.hummel@gmail.com>
2019-03-15 18:08:45 +0100

    [FEATURE] init

---
+++ README.md
@@ -0,0 +1,19 @@
+# xAPI to CSV for xAPI-Probe and TLA
+
+Converts JSON dumps from Trusted Learning Analytics to CSV for convenience.
+
+## Usage
+
+With Python >3.5 installed, run:
+
+#### Linux
+
+```bash
+python main.py facts.log out.csv
+```
+
+#### Windows
+
+```bash
+py main.py facts.log out.csv
+```
---
+++ main.py
@@ -0,0 +1,76 @@
+import argparse
+import csv
+import json
+
+__author__ = "Noah Hummel"
+parser = argparse.ArgumentParser(
+    description="Convert xAPI-Probe xAPI statements to CSV"
+)
+parser.add_argument(
+    "dump",
+    type=str,
+    help="Kubernetes namespace of the deployment"
+)
+
+parser.add_argument(
+    "out",
+    type=str,
+    help="Kubernetes namespace of the deployment"
+)
+
+args = parser.parse_args()
+
+with open(args.dump) as f:
+    statements = [json.loads(line)["data"]["fact"]
+                  for line in f.read().split("\n") if line]
+
+answers = list(filter(
+    lambda s: s["verb"]["id"] == "http://adlnet.gov/expapi/verbs/answered",
+    statements
+))
+
+FIELD_NAMES = ["subject", "question_id", "dimension_id", "survey_id",
+               "scale_start", "scale_end", "scale_step_size", "answer_value",
+               "question_texts", "dimension_texts", "survey_texts"]
+
+def to_csv_row(answer):
+    subject = answer["actor"]["name"]
+    question = answer["object"]
+    dimension = answer["context"]["contextActivities"]["parent"][0]
+    survey = answer["context"]["contextActivities"]["grouping"][0]
+
+    question_texts = question["definition"]["name"]
+    dimension_texts = dimension["definition"]["name"]
+    survey_texts = survey["definition"]["name"]
+
+    question_id = question["id"]
+    dimension_id = dimension["id"]
+    survey_id = survey["id"]
+
+    score = answer["result"][0]["score"]
+    scale_start = score["min"]
+    scale_end = score["max"]
+    scale_step_size = score["scaled"]
+    answer_value = score["raw"]
+
+    return {
+        "subject": subject,
+        "question_id":  question_id,
+        "dimension_id": dimension_id,
+        "survey_id": survey_id,
+        "scale_start": scale_start,
+        "scale_end": scale_end,
+        "scale_step_size": scale_step_size,
+        "answer_value": answer_value,
+        "question_texts": question_texts,
+        "dimension_texts": dimension_texts,
+        "survey_texts": survey_texts
+    }
+
+
+with open(args.out, 'w+', newline='') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=FIELD_NAMES)
+
+    writer.writeheader()
+    for a in answers:
+        writer.writerow(to_csv_row(a))
